213fa9e0db8fcf7d8ad205de9365694110e65e6d
EarlGimenez <earl.reynan123@gmail.com>
2025-07-08 13:32:33 +0800

    Initial Views Created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Livewire\Products\Index as ProductsIndex;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ProductsIndex::class);
+
+
+Route::resource('products', ProductController::class);
